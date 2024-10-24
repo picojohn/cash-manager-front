3349f2cfbe19e4f27e8325ea73403554726c3e07
Santi6957 <daniel.alvarez63@misena.edu.co>
2024-10-24 11:52:50 -0500

    se globaliza el css

--- src/app/modules/configuration/currency/currency.component.ts
+++ src/app/modules/configuration/currency/currency.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
 import { ErrorService } from 'src/app/shared/services/error.service';
